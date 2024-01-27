855d5153be5f1102d01e8078af2c78b1bdf6bcb4
Sean Park <seanpark132@gmail.com>
2024-01-27 13:43:45 -0800

    updated searchParams logic, render logic

---
+++ src/pages/WorldMaps.tsx
@@ -0,0 +1,46 @@
+import { useSearchParams } from "react-router-dom";
+import LinkArea from "../components/LinkArea";
+import { ALL_WORLD_MAPS_DATA } from "../data/AllWorldMaps.ts";
+import RegionSelect from "../components/RegionSelect";
+
+export default function WorldMaps() {
+  const [searchParams, setSearchParams] = useSearchParams({
+    worldMap: "",
+    parentWorld: "",
+    map: "",
+  });
+
+  const worldMap = searchParams.get("worldMap");
+  const parentWorld = searchParams.get("parentWorld");
+  const map = searchParams.get("map");
+
+  const imageName = `${parentWorld ? parentWorld : "None"}_${worldMap}.jpg`;
+
+  if (!worldMap) {
+    return <RegionSelect setSearchParams={setSearchParams} />;
+  }
+
+  const worldMapData = ALL_WORLD_MAPS_DATA.find(
+    (worldMapData) => worldMapData.raw.worldMapName === worldMap,
+  );
+
+  const linksArray = worldMapData?.raw.links;
+
+  return (
+    <main className="mt-8">
+      <h1 className="mb-2">Select an area:</h1>
+      <div className="relative">
+        <img src={`/world_maps/${imageName}`} />
+        {linksArray?.map((link) => (
+          <LinkArea
+            worldMap={link.linksTo}
+            base64ImgCode={link.linkImage.image}
+            x={link.linkImage.origin.value.x}
+            y={link.linkImage.origin.value.y}
+            setSearchParams={setSearchParams}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
