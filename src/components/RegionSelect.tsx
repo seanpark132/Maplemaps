1d39a473f9f713283ed05c0d49ea2892508931b3
Sean Park <seanpark132@gmail.com>
2024-01-27 13:41:06 -0800

    added grandis to RegionSelect

--- src/components/RegionSelect.tsx
+++ src/components/RegionSelect.tsx
@@ -9,19 +9,20 @@ export default function RegionSelect(props: Props) {
     <main className="mt-8">
       <h1 className="mb-4">Select a Region:</h1>
       <div className="grid grid-cols-2 gap-16">
-        <button onClick={() => handleClick("arcane_river")}>
+        {/* <button onClick={() => handleClick("arcane_river")}>
           <img className="" src="/regions/arcane_river.jpg" />
-        </button>
-        <button onClick={() => handleClick("grandis")}>
-          <img src="/regions/grandis.jpg" />
+        </button> */}
+        <button onClick={() => handleClick("CGWorldMap", "GWorldMap")}>
+          <img src="/world_maps/GWorldMap_CGWorldMap.jpg" />
         </button>
       </div>
     </main>
   );
 
-  function handleClick(region: string) {
+  function handleClick(worldMap: string, parentWorld: string) {
     props.setSearchParams((prev) => {
-      prev.set("region", region);
+      prev.set("worldMap", worldMap);
+      prev.set("parentWorld", parentWorld);
       return prev;
     });
   }
