2a71c0a4a2d5752df2d16e6404c5ae5efe33f81b
Sean Park <seanpark132@gmail.com>
2024-01-29 16:50:58 -0800

    added WorldMap033 to WORLD_MAP_OFFSETS

--- src/GlobalVariables.tsx
+++ src/GlobalVariables.tsx
@@ -5,6 +5,7 @@ const WORLD_MAP_OFFSETS = {
   GWorldMap: { x: 2, y: 4 },
   WorldMap280: { x: 3, y: 3 },
   WorldMap290: { x: 4, y: 4 },
+  WorldMap033: { x: 6, y: 6 },
 };
 
 const LINK_AREA_Z_INDEX = ["WorldMap030", "WorldMap170"];
