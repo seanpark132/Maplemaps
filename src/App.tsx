31c96b40d69204eb608e24c50a5fe04266d09682
Sean Park <seanpark132@gmail.com>
2024-01-30 05:37:37 -0800

    changed world_maps data reference to netlify function endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./App.css";
 import WorldMaps from "./pages/WorldMaps";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [allWorldMapsData, setAllWorldMapsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/.netlify/functions/getWorldMapsData");
+        const data = await response.json();
+        setAllWorldMapsData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="flex flex-col items-center justify-center px-6 py-6">
         <Routes>
-          <Route index element={<WorldMaps />} />
+          <Route
+            index
+            element={<WorldMaps allWorldMapsData={allWorldMapsData} />}
+          />
         </Routes>
       </div>
     </>
