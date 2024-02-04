8b64eca9a330c8c5a30ec1e0c88add408139e2e9
Sean Park <seanpark132@gmail.com>
2024-02-04 13:17:07 -0800

    added mob level, name to MapDotHover component

--- src/components/MapDotHover.tsx
+++ src/components/MapDotHover.tsx
@@ -1,3 +1,5 @@
+import { TEMP_MOBS_DATA } from "../TEMP_mobsData";
+
 type Props = {
   mapNumber: number;
   mapsData: any[];
@@ -5,12 +7,21 @@ type Props = {
 
 export default function MapDotHover(props: Props) {
   const mapData = props.mapsData.find((map) => map.map_id === props.mapNumber);
+  const uniqueMobIds = [
+    ...new Set(mapData?.raw.mobs.map((mob: any) => mob.id)),
+  ];
+  const uniqueMobData = uniqueMobIds.map((mobId) =>
+    TEMP_MOBS_DATA.find((mob) => mobId === mob.mob_id),
+  );
 
   return (
     <>
       {mapData && (
-        <span className="map-dot-hover-container absolute bottom-1/2 left-1/2 z-20 w-fit min-w-16 rounded-md border-black bg-black p-2">
-          <p className="text-nowrap text-xs">{mapData.map_name}</p>
+        <span className="map-dot-hover-container bg-black-transparent-75 absolute bottom-1/2 left-1/2 z-20 w-fit min-w-16 rounded-md border border-white p-2">
+          <p className="text-nowrap text-xs">{mapData.raw.name}</p>
+          {uniqueMobData.map((mob: any) => (
+            <p className="text-nowrap text-xs text-red-500">{`${mob.raw.name} (${mob.raw.meta.level})`}</p>
+          ))}
         </span>
       )}
     </>
