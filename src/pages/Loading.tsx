c532ce6c6083551b78719849c9763499c5fb6ae9
Sean Park <seanpark132@gmail.com>
2024-02-07 19:09:07 -0800

    Added loading, nopage pages

---
+++ src/pages/Loading.tsx
@@ -0,0 +1,7 @@
+export default function NoPage() {
+  return (
+    <main>
+      <h1 className="p-6">Loading...</h1>
+    </main>
+  );
+}
---
+++ src/pages/NoPage.tsx
@@ -0,0 +1,7 @@
+export default function NoPage() {
+  return (
+    <main>
+      <h1 className="p-6">Error 404: Page not found.</h1>
+    </main>
+  );
+}
