35342129dca9a6269e32b6a0d28a5c44140dbfeb
Sean Park <seanpark132@gmail.com>
2024-02-12 21:22:07 -0800

    installed react-query and react-query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import Navbar from "./components/Navbar.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Navbar />
       <App />
+      <ReactQueryDevtools />
     </BrowserRouter>
-  </React.StrictMode>,
+  </QueryClientProvider>,
 );
