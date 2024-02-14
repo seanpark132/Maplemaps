07a02d89a1099e4f703f8b7a96cd5476f7464b21
Sean Park <seanpark132@gmail.com>
2024-02-14 10:37:12 -0800

    refactored App file (no mapInfo routes yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect, lazy, Suspense } from "react";
+import { useState, lazy, Suspense } from "react";
 import "./App.css";
 import WorldMaps from "./pages/WorldMaps";
-import { WorldMapData } from "./types/worldMapTypes";
-import { TEMP_WORLD_MAPS_DATA } from "./TEMP_worldMapsData";
 import NoPage from "./pages/NoPage";
 import Loading from "./pages/Loading";
 import { MapData } from "./types/mapTypes";
+import { MobData } from "./types/mobTypes";
 
 const MapInfo = lazy(() => import("./pages/MapInfo"));
 
 function App() {
-  const [worldMapsData, setWorldMapsData] = useState<WorldMapData[]>([]);
-  const [mapsData, setMapsData] = useState<MapData[]>([]);
-
-  useEffect(() => {
-    //   const fetchWorldMapsData = async () => {
-    //     try {
-    //       const response = await fetch("/.netlify/functions/getWorldMapsData");
-    //       const data = await response.json();
-    //       setWorldMapsData(data);
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   };
-
-    const fetchMapsData = async () => {
-      try {
-        const response = await fetch("/.netlify/functions/getMapsData");
-        const data = await response.json();
-        setMapsData(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    // fetchWorldMapsData();
-    setWorldMapsData(TEMP_WORLD_MAPS_DATA);
-    fetchMapsData();
-  }, []);
-
-  if (!mapsData || !worldMapsData) {
-    return <Loading />;
-  }
+  const [mapsData, setMapsData] = useState<Record<number, MapData>>({});
+  const [mobsData, setMobsData] = useState<Record<number, MobData>>({});
 
   return (
     <>
@@ -52,10 +21,16 @@ function App() {
             <Route
               index
               element={
-                <WorldMaps worldMapsData={worldMapsData} mapsData={mapsData} />
+                <WorldMaps
+                  mapsData={mapsData}
+                  mobsData={mobsData}
+                  setMapsData={setMapsData}
+                  setMobsData={setMobsData}
+                />
               }
             />
-            {mapsData &&
+            {/* likely need an array of just map_ids*/}
+            {/* {mapsData &&
               mapsData.map((map) => (
                 <Route
                   key={map.map_id}
@@ -66,7 +41,7 @@ function App() {
                     </Suspense>
                   }
                 />
-              ))}
+              ))} */}
             <Route path="*" element={<NoPage />} />
           </Routes>
         </Suspense>
