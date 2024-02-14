67cd0255146064a5ef004d55e0360e020299a59f
Sean Park <seanpark132@gmail.com>
2024-02-14 10:29:50 -0800

    Changed WorldMaps to fetch worldMaps by region

--- src/pages/WorldMaps.tsx
+++ src/pages/WorldMaps.tsx
@@ -1,17 +1,30 @@
 import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import Loading from "./Loading.tsx";
 import LinkArea from "../components/LinkArea";
 import MapDot from "../components/MapDot.tsx";
 import RegionSelect from "../components/RegionSelect";
-import { GOOGLE_CLOUD_IMAGE_URL } from "../GlobalVariables.tsx";
+import { GOOGLE_CLOUD_IMAGE_URL } from "../utils/GlobalVariables.ts";
 import { Link, Map, WorldMapData } from "../types/worldMapTypes.ts";
 import { MapData } from "../types/mapTypes.ts";
+import { MobData } from "../types/mobTypes.ts";
+import Error from "./Error.tsx";
+import { useFetchWorldMapsData } from "../hooks/useFetchWorldMapsData.tsx";
 
 type Props = {
-  worldMapsData: WorldMapData[];
-  mapsData: MapData[];
+  mapsData: Record<number, MapData>;
+  mobsData: Record<number, MobData>;
+  setMapsData: React.Dispatch<React.SetStateAction<Record<number, MapData>>>;
+  setMobsData: React.Dispatch<React.SetStateAction<Record<number, MobData>>>;
 };
 
 export default function WorldMaps(props: Props) {
+  const [worldMapsData, setWorldMapsData] = useState<
+    Record<string, WorldMapData>
+  >({});
+  const [visitedWorldMaps, setVisitedWorldMaps] = useState<Set<string>>(
+    new Set(),
+  );
   const [searchParams, setSearchParams] = useSearchParams({
     worldMap: "",
     parentWorld: "",
@@ -20,50 +33,55 @@ export default function WorldMaps(props: Props) {
   const parentWorld = searchParams.get("parentWorld");
   const imageName = `${parentWorld ? parentWorld : "None"}_${worldMap}.webp`;
 
+  useFetchWorldMapsData(
+    worldMap,
+    worldMapsData,
+    setWorldMapsData,
+    setVisitedWorldMaps,
+  );
+
   if (!worldMap) {
     return <RegionSelect setSearchParams={setSearchParams} />;
   }
 
-  const worldMapData = props.worldMapsData.find(
-    (data) => data.worldMapName === worldMap,
-  );
-  const parentWorldData = props.worldMapsData.find(
-    (data) => data.worldMapName === parentWorld,
-  );
+  const worldMapData = worldMapsData[worldMap];
+  const parentWorldData = parentWorld ? worldMapsData[parentWorld] : null;
   const parentParentWorld = parentWorldData?.parentWorld;
 
   return (
-    <main className="flex min-w-[40rem] flex-col items-center pt-8">
-      <h1 className="mb-2">Select a map/area:</h1>
-      <div
-        className="relative"
-        onContextMenu={(event) => handleRightClick(event)}
-      >
-        <img
-          src={`${GOOGLE_CLOUD_IMAGE_URL}/world_maps/${imageName}`}
-          width={640}
-          height={470}
-          alt={`World map image: ${worldMap}`}
-        />
-        {worldMapData?.links.map((link: Link) => (
-          <LinkArea
-            key={link.linksTo}
-            link={link}
-            worldMapsData={props.worldMapsData}
-            currentWorldMap={worldMap}
-            setSearchParams={setSearchParams}
+    worldMapData && (
+      <main className="flex min-w-[40rem] flex-col items-center pt-8">
+        <h1 className="mb-2">Select a map/area:</h1>
+        <div
+          className="relative"
+          onContextMenu={(event) => handleRightClick(event)}
+        >
+          <img
+            src={`${GOOGLE_CLOUD_IMAGE_URL}/world_maps/${imageName}`}
+            width={640}
+            height={470}
+            alt={`World map image: ${worldMap}`}
           />
-        ))}
-        {worldMapData?.maps.map((map: Map) => (
-          <MapDot
-            key={`${map.x}${map.y}${map.type}`}
-            currentWorldMap={worldMap}
-            map={map}
-            mapsData={props.mapsData}
-          />
-        ))}
-      </div>
-    </main>
+          {worldMapData.links.map((link: Link) => (
+            <LinkArea
+              key={link.linksTo}
+              link={link}
+              worldMapsData={worldMapsData}
+              currentWorldMap={worldMap}
+              setSearchParams={setSearchParams}
+            />
+          ))}
+          {worldMapData.maps.map((map: Map) => (
+            <MapDot
+              key={`${map.x}${map.y}${map.type}`}
+              currentWorldMap={worldMap}
+              map={map}
+              mapsData={props.mapsData}
+            />
+          ))}
+        </div>
+      </main>
+    )
   );
 
   function handleRightClick(event: any) {
--- src/types/worldMapTypes.ts
+++ src/types/worldMapTypes.ts
@@ -13,6 +13,7 @@ export type Link = {
 };
 
 export type Map = {
+  noTooltip?: boolean;
   description?: string;
   type: number;
   x: number;
