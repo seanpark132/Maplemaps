e123534c1bd8fb9f8f20221c042b1aa76f026a25
Sean Park <seanpark132@gmail.com>
2024-02-14 19:15:51 -0800

    removed react-query related code

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import Navbar from "./components/Navbar.tsx";
 import "./index.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
+  <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <App />
-      <ReactQueryDevtools />
     </BrowserRouter>
-  </QueryClientProvider>,
+  </React.StrictMode>,
 );
