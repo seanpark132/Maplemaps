ba73177c223bd3ecba0729c1ff22a8e7b7117ff6
Sean Park <seanpark132@gmail.com>
2024-03-09 16:53:08 -0800

    fixed MobInfo margins for when there's more than 1 mob

--- src/components/MobInfo.tsx
+++ src/components/MobInfo.tsx
@@ -21,14 +21,14 @@ export default function MobInfo(props: Props) {
 
   return (
     <div className="flex items-center py-4 lg:py-6">
-      <div className="flex max-w-20 flex-col items-center justify-center text-center md:max-w-36">
+      <div className="mr-4 flex max-w-20 flex-1 flex-col items-center justify-center text-center md:mr-6 md:max-w-36">
         <img
           src={`${GOOGLE_CLOUD_IMAGE_URL}/raw/mobs/${props.mobData.mob_id}.png`}
           className="max-w-20 md:max-w-36"
         />
         <p className="mt-4 font-semibold">{props.mobData.name}</p>
       </div>
-      <div className="pl-4 lg:pl-8">
+      <div className="ml-auto">
         <InfoGrid descriptions={descriptions} values={values} />
       </div>
     </div>
